d24fddbc7efd68dcb318389043f1ae4c7c4d2496
ipreppcode <132910855+ipreppcode@users.noreply.github.com>
2025-12-16 02:05:18 +0530

    Update CFTool.hpp

--- src/Extensions/CFTool.hpp
+++ src/Extensions/CFTool.hpp
@@ -1,18 +1,8 @@
 /*
  * Copyright (C) 2019-2021 Ashar Khan <ashar786khan@gmail.com>
+ * Modified for Browser-Based Submission
  *
  * This file is part of CP Editor.
- *
- * CP Editor is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * I will not be responsible if CP Editor behaves in unexpected way and
- * causes your ratings to go down and or lose any important contest.
- *
- * Believe Software is "Software" and it isn't immune to bugs.
- *
  */
 
 #ifndef CFTOOL_HPP
@@ -47,11 +37,13 @@ class CFTool : public QObject
   private:
     QString problemContestId, problemCode, lastStatus;
     QProcess *CFToolProcess = nullptr;
+    QProcess *browserAutomation = nullptr;
     MessageLogger *log;
     QString CFToolPath;
 
     void showToastMessage(const QString &message);
     QString getCFToolVersion() const;
+    void automateSubmission(const QString &url, const QString &sourceCode);
 };
 } // namespace Extensions
 
